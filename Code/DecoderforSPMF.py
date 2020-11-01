c9dedcb7e3d8092ab0c5ad2bc09351994a7cb77f
zaboura <abdelhakzabour@gmail.com>
2020-11-01 12:22:30 +0100

    First commit

---
+++ Code/DecoderforSPMF.py
@@ -0,0 +1,46 @@
+def decode(a):
+#    print invdico
+    list=a.split(' ')
+    s=""
+ #   print list
+    for i in list:
+ #       print "xxx"+i+"xxxx"
+        if i!='' and int(i) in invdico.keys():
+ #           print "oui"
+            s=s+invdico[int(i)]+' '
+ #   print "s"
+ #   print s
+    return s
+
+p=len(sys.argv)
+#print p
+
+if p==3:
+    f_out=sys.argv[2]
+else:
+    f_out="res_decode.txt"
+res=open(f_out,'w')
+
+    
+# Construction du dictionnaire
+dic = open('invdico.dbm', 'rb')
+invdico = pickle.load(dic)
+#print invdico
+
+results= sys.argv[1]
+f = open(results,'r')
+
+for l in f.readlines():
+    r=l.split("#")
+ #   print r
+    aa=r[0].split('==> ') # on decoupe comme s'il s'agit d'une regle
+    if len(aa)==1:
+        rdec=decode(aa[0])
+        res.write(rdec+'# '+'# '.join(r[1:])+'\n')
+    elif len(aa)==2:
+        rdec0=decode(aa[0])
+        rdec1=decode(aa[1])
+        res.write(rdec0 +'==> '+ rdec1 +'# '+'# '.join(r[1:])+'\n')
+    else:
+        print('!!!!! ligne non conforme')
+res.close()
---
+++ Code/Encode_data_forSPMF.py
@@ -0,0 +1,71 @@
+# import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+import sys
+import pickle
+import re
+import os
+
+
+p=len(sys.argv)
+print(p)
+
+if p==3:
+    f_out=sys.argv[2]
+else:
+    f_out="out.txt"
+
+# Construction du dictionnaire
+dico={}
+invdico={}
+indice=1
+
+data = pd.read_csv(sys.argv[1], sep = ';')
+
+# Feature selection 
+features = ['REGION', 'AGER20', 'ANARR', 'ANEMR', 'ASCEN', 'BAIN', 'BATI', 'CATL', 'CHOS',
+            'CLIM', 'CMBL', 'COUPLE', 'CS1', 'CUIS', 'DEROU', 'DIPL_15', 'EAU', 'EGOUL',
+            'ELEC', 'EMPL', 'ETUD', 'GARL', 'ILTUU', 'IMMI', 'INAT', 'INFAM', 'INPER',
+            'MOCO', 'MODV', 'NA38', 'NATC', 'NATNC', 'NBPI', 'NPERR', 'RECH', 'SANI',
+            'SEXE', 'SFM', 'STOCD', 'TACT', 'VOIT', 'WC']
+# convert all the values to obeject
+data = data[features].astype('str')
+# select smmal set from data
+data = data[features].iloc[:10]
+
+for i, row in enumerate(data.values):
+    for col, val in zip(features,row):
+        key = col +"-"+ val
+        data[col].iloc[i] = key
+        if key not in dico.keys():
+            dico[key] = indice
+            invdico[indice] = key
+            indice += 1
+            
+print(dico)
+
+
+    
+with open('encoded_file.txt', 'w') as out: 
+
+    
+    for row in data.values:
+        rows = []
+        for val in row:
+
+            rows.append(dico[val])
+        rows.sort()
+        l=[str(i) for i in rows]
+        out.write(" ".join(l))
+        out.write("\n")
+    out.close()
+
+
+fichier=open('invdico.dbm', 'wb')
+pickle.dump(invdico,fichier)
+
+
+
+
+
